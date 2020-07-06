681efa2a5d8a8b4a374bb58d40a1f26b5b2bf337
上田智也 <tomo@uedatomoyanoMacBook-Pro.local>
2020-07-07 01:35:36 +0900

    4-8-1

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,51 @@
 class BooksController < ApplicationController
-  def index
-  end
 
   def show
+    @book = Book.find(params[:id])
+  end
+
+  def index
+  	@books = Book.all
+  	@book = Book.new
   end
 
   def new
+    @book = Book.new
+  end
+
+  def create
+    book = Book.new(book_params)
+    if book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(book.id)
+    else
+    render action: :index
+    end
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    flash[:notice] = "Book was successfully update."
+    redirect_to book_path(book)
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    if book.destroy
+      flash[:notice] = "Book was successfully destroy."
+      redirect_to books_path
+    else
+      render action: :index
+    end
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
