b1e8f32ae624cbe4e736e33790e8237d13cc5de8
上田智也 <tomo@uedatomoyanoMacBook-Pro.local>
2020-07-07 15:50:26 +0900

    [Finish] 4-8-4

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -14,20 +14,24 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-    if book.save
+    @book = Book.new(book_params)
+    if @book.save
       flash[:notice] = "Book was successfully created."
-      redirect_to book_path(book.id)
+      redirect_to book_path(@book.id)
     else
-      render 'index'
+      @books = Book.all
+      render action: :index
     end
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    flash[:notice] = "Book was successfully update."
-    redirect_to book_path(book)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "Book was successfully update."
+      redirect_to book_path(@book)
+    else
+      render action: :edit
+    end
   end
 
   def edit
